f4f302dc5242d568cc4270a96f0f87a71e2807f5
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-06-20 04:23:01 -0400

    add array exercise 05

---
+++ src/test/java/datastructure/array/ArrayPractice_05.java
@@ -0,0 +1,21 @@
+package datastructure.array;
+
+public class ArrayPractice_05 {
+
+	// Check if an array contains a specific value.
+	public static boolean containsValue(int[] arr, int value) {
+		for (int num : arr) {
+			if (num == value) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 5, 7, 2, 4, 9 };
+		System.out.println("Contains 2: " + containsValue(array, 2));
+		System.out.println("Contains 3: " + containsValue(array, 3));
+	}
+
+}
