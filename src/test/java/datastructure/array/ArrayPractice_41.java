d8ec2586e6203243c2c7def06ecbf78bc55a9fa8
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-06-21 11:49:20 -0400

    add array exercise 41

--- src/test/java/datastructure/array/ArrayPractice_41.java
+++ src/test/java/datastructure/array/ArrayPractice_41.java
@@ -4,23 +4,11 @@ import java.util.Arrays;
 
 public class ArrayPractice_41 {
 
-	// Implement the Insertion Sort algorithm.
-	public static void insertionSort(int[] arr) {
-		int n = arr.length;
-		for (int i = 1; i < n; i++) {
-			int key = arr[i];
-			int j = i - 1;
-			while (j >= 0 && arr[j] > key) {
-				arr[j + 1] = arr[j];
-				j--;
-			}
-			arr[j + 1] = key;
-		}
-	}
+	// Implement the Merge Sort algorithm.
+	
 
 	public static void main(String[] args) {
-		int[] arrForInsertionSort = { 22, 11, 99, 88, 9, 7, 42 };
-		insertionSort(arrForInsertionSort);
-		System.out.println("Insertion Sorted: " + Arrays.toString(arrForInsertionSort));
+
+
 	}
 }
