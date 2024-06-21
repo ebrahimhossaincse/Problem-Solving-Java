550f62f46179226fa8b5a141e4b54008b7091ff4
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-06-21 08:39:59 -0400

    add array exercise 38

---
+++ src/test/java/datastructure/array/ArrayPractice_38.java
@@ -0,0 +1,26 @@
+package datastructure.array;
+
+import java.util.Arrays;
+
+public class ArrayPractice_38 {
+
+	// Implement the Bubble Sort algorithm
+	public static void bubbleSort(int[] arr) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] arrForSorting = { 5, 2, 9, 1, 5, 6 };
+		bubbleSort(arrForSorting);
+		System.out.println("Bubble Sorted: " + Arrays.toString(arrForSorting));
+	}
+}
